be59a0f19b5846002be0896b306a3726310fdb21
Kežić Nenad <kezicnenad@gmail.com>
2022-05-21 15:37:40 +0200

    Server

--- models/artikliModel.js
+++ models/artikliModel.js
@@ -6,96 +6,20 @@ export const ArtiklSchema = new Schema({
     type: String,
     required: "Unesi naziv",
   },
-  vlasnik: {
-    type: String,
-    required: "Unesi vlasnika",
-  },
-  postanski_broj: {
-    type: Number,
-    required: "Unesi broj pošte",
-  },
-  mjesto: {
-    type: String,
-    required: "Unesi mjesto",
-  },
-  adresa: {
-    type: String,
-    required: "Unesi adresu",
-  },
   opis: {
     type: String,
-    required: "Unesi vlasnika",
+    required: "Unesi opis",
   },
-  telefon: {
-    type: Number,
-    required: "Unesi vlasnika",
-  },
-  mail: {
+  slika: {
     type: String,
-    required: "Unesi mail",
+    required: "Unesi sliku",
   },
-  web: {
-    type: String,
-    required: "Unesi web adresu",
-  },
-  zvjezdice: {
+  kolicina: {
     type: Number,
-    required: "Unesi broj zvjezdica",
+    required: "Unesi kolicinu",
   },
-  ocjena: {
+  cijena: {
     type: Number,
-    required: "Unesi ocjenu",
-  },
-  povrsina: {
-    type: Number,
-    required: "Unesi povrsinu",
-  },
-  broj_soba: {
-    type: Number,
-    required: "Unesi broj soba",
-  },
-  broj_kreveta: {
-    type: Number,
-    required: "Unesi broj kreveta",
-  },
-  kuhinja: {
-    type: Boolean,
-    required: "Kuhinja",
-  },
-  perilica: {
-    type: Boolean,
-    required: "Perilica",
-  },
-  parking: {
-    type: Boolean,
-    required: "Parking",
-  },
-  wifi: {
-    type: Boolean,
-    required: "WiFi",
-  },
-  ljubimci: {
-    type: Boolean,
-    required: "Ljubimci",
-  },
-  etaza: {
-    type: Number,
-    required: "Unesi etažu",
-  },
-  dizalo: {
-    type: Boolean,
-    required: "Dizalo",
-  },
-  galerija: {
-    type: Number,
-    required: "Unesi galeriju",
-  },
-  blizina: {
-    type: Number,
-    required: "Unesi objekte u blizini",
-  },
-  recenzije: {
-    type: Number,
-    required: "Unesi recenziju",
-  },
+    required: "Unesi cijenu",
+  }
 });
